4da6b27a9584b9788b9e0a3dc6236b455baa9a5c
phanhuy1 <75444611+phanhuy1@users.noreply.github.com>
2023-03-30 22:13:00 +0700

    Update request permission for read/write images

--- app/src/main/java/com/example/sevenalbum/activities/mainActivities/ListAlbumsActivity.java
+++ app/src/main/java/com/example/sevenalbum/activities/mainActivities/ListAlbumsActivity.java
@@ -1,17 +1,26 @@
 package com.example.sevenalbum.activities.mainActivities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 
 import com.example.sevenalbum.R;
 
 public class ListAlbumsActivity extends AppCompatActivity {
     private Toolbar toolbar;
-
+    String[] permissions= {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,20 @@ public class ListAlbumsActivity extends AppCompatActivity {
         toolbar=findViewById(R.id.list_album_bar);
 
         setSupportActionBar(toolbar);
+        requestPermissions(permissions, 80);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==80){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
