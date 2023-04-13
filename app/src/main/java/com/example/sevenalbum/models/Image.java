6a5142a41fa71c25c134dc4317544d32f5ae06a6
huykevin2002 <giahuy300336@gmail.com>
2023-04-13 15:17:15 +0700

    model Image

--- app/src/main/java/com/example/sevenalbum/models/Image.java
+++ app/src/main/java/com/example/sevenalbum/models/Image.java
@@ -1,2 +1,37 @@
-package com.example.sevenalbum.models;public class Image {
+package com.example.sevenalbum.models;
+public class Image {
+    private String path;
+    private String thumb;
+    private String dateTaken;
+    public String getPath() {
+        return path;
+    }
+
+    public Image() {
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getThumb() {
+        return thumb;
+    }
+
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
+    }
+
+    public Image(String path, String thumb) {
+        this.path = path;
+        this.thumb = thumb;
+    }
+
+    public String getDateTaken() {
+        return dateTaken;
+    }
+
+    public void setDateTaken(String dateTaken) {
+        this.dateTaken = dateTaken;
+    }
 }
