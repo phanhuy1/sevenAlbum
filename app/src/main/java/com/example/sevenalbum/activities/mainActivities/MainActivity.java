3c58a37edd161b94fce47c83cc7df684b9be2a14
phanhuy1 <75444611+phanhuy1@users.noreply.github.com>
2023-04-13 16:19:39 +0700

    add mainActivities MainActivity.java

--- app/src/main/java/com/example/sevenalbum/activities/mainActivities/MainActivity.java
+++ app/src/main/java/com/example/sevenalbum/activities/mainActivities/MainActivity.java
@@ -1,15 +1,143 @@
 package com.example.sevenalbum.activities.mainActivities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.sevenalbum.R;
+import com.example.sevenalbum.adapters.ViewPagerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.karan.churi.PermissionManager.PermissionManager;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    ViewPager2 viewPager;
+    PermissionManager permission;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Hide status bar
+
+        /*getSupportActionBar();*/
         setContentView(R.layout.activity_main);
+
+
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        viewPager = findViewById(R.id.view_pager);
+
+        permission = new PermissionManager() {
+            @Override
+            public void ifCancelledAndCannotRequest(Activity activity) {
+            }
+        };
+        permission.checkAndRequestPermissions(this);
+        setUpViewPager();
+        loadSettings(); // doesn't work?
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.photo:
+
+                        viewPager.setCurrentItem(0);
+                        break;
+
+                    case R.id.album:
+
+                        viewPager.setCurrentItem(1);
+                        break;
+
+                    case R.id.scret:
+
+                        viewPager.setCurrentItem(2);
+                        break;
+
+                    case R.id.favorite:
+
+                        viewPager.setCurrentItem(3);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
+
     }
+
+
+    // Toolbar handle
+
+
+    private void setUpViewPager() {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPagerAdapter.setContext(getApplicationContext());
+        viewPager.setAdapter(viewPagerAdapter);
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                switch (position){
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.photo).setChecked(true);
+                        break;
+                    case 1:
+
+                        bottomNavigationView.getMenu().findItem(R.id.album).setChecked(true);
+                        break;
+                    case 2:
+
+                        bottomNavigationView.getMenu().findItem(R.id.scret).setChecked(true);
+                        break;
+                    case 3:
+
+                        bottomNavigationView.getMenu().findItem(R.id.favorite).setChecked(true);
+                        break;
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        permission.checkResult(requestCode, permissions, grantResults);
+    }
+
+
+    private void loadSettings(){
+        PreferenceManager.setDefaultValues(this, R.xml.setting, false);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+
 }
